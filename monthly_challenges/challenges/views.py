0d253d6488b64ef511b7e156ef35dc7c0ecb4c29
Kriva <edronbaton@proton.me>
2024-11-23 21:42:48 +0100

    monthly_challenges dynamic path segments & captured values

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
 def january(request):
@@ -8,3 +8,15 @@ def january(request):
 
 def february(request):
     return HttpResponse("Февраль по английски будет february")
+
+def monthly_challenges(request, month):
+    challenge_text = None
+
+    if month == 'january':
+        challenge_text = "Не есть мясо целый месяц"
+    elif month == 'february':
+        challenge_text = "Пробежка по утрам"
+    else:
+        return HttpResponseNotFound("Этот месяц не поддерживается")
+    
+    return HttpResponse(challenge_text)
